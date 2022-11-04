a415b874320c67136842879c68dd6cbd026ef055
SunSeekerX <1647800606@qq.com>
2022-11-04 18:04:11 +0800

    feat: feat

--- src/js/app.js
+++ src/js/app.js
@@ -53,7 +53,18 @@ $(document).ready(() => {
   }
 
   // 评论
-  if (config.valineOptions.open) {
-    initValine(config.valineOptions)
+  // if (config.valineOptions.open) {
+  //   initValine(config.valineOptions)
+  // }
+
+  // twikoo
+  if ($('#tcomment').length !== 0 && window._ghost_env_twikoo_id) {
+    twikoo.init({
+      envId: window._ghost_env_twikoo_id, // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
+      el: '#tcomment', // 容器元素
+      // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，腾讯云环境填 ap-shanghai 或 ap-guangzhou；Vercel 环境不填
+      // path: location.pathname, // 用于区分不同文章的自定义 js 路径，如果您的文章路径不是 location.pathname，需传此参数
+      // lang: 'zh-CN', // 用于手动设定评论区语言，支持的语言列表 https://github.com/imaegoo/twikoo/blob/main/src/client/utils/i18n/index.js
+    })
   }
 })
